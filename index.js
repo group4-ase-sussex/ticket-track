aee47c80c9e56b54e370cf2584ad3537ffe9b993
Lawrence Kyalo <lawrencekkyalo@gmail.com>
2019-02-27 14:18:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+//index.js
+const cron = require("node-cron");
+const express = require("express");
+let nodemailer = require("nodemailer");
+
+app = express();
+
+//create mail transporter
+let transporter = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+		user:"sampleEmail@gmail.com",
+		pass:"myPassword"
+	}
+});
+
+//sending emails at periodic intervals
+cron.schedule("* * * * Wednesday", function(){
+	console.log("----------------------");
+	console.log("Running Cron Job");
+	let mailOptions = {
+		from: "senderEmail@gmail.com",
+		to: "recipientEmail@gmail.com",
+		subject: `CRON-JOB 001`,
+		text: `Hi there, this is a test email form our cron job.`
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			throw error;
+		} else {
+			console.log("Email sent successfully");
+		}
+	});
+});
+
+app.listen("3128");
